c32de5b80132949772f3a23bf60101db350d32d4
trentb10 <trentbmin@gmail.com>
2021-05-01 04:11:26 -0400

    Slightly modified code formatting.

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,22 +31,26 @@ export default function Home() {
 				<h2>Welcome to my home page!</h2>
 					
 				<p>
-					I am a graduate from Centennial College with an Advanced Diploma in Software Engineering Technology and 
-					from the University of Toronto with an Honours Bachelor of Arts, majoring in Media Studies and minoring in 
-					English and Psychology.
+					I am a graduate from Centennial College with an Advanced Diploma 
+					in Software Engineering Technology and from the University of 
+					Toronto with an Honours Bachelor of Arts, majoring in Media 
+					Studies and minoring in English and Psychology.
 				</p>
 
 				<p>
-					The home page you are looking at right now was built as part as an assignment for COMP213 at Centennial 
-					College, and it was hosted on <a href="http://studentweb.cencol.ca/">studentweb.cencol.ca</a>. I have now
-					expanded it into my own little site to host some of my past works and assignments I did as a student at 
-					Centennial and UofT; a writer with 24OurMusic, Amp the Sound, and Feed My Curiosity, which I 
-					am currently writing scripts for; and of course, my resume!
+					The home page you are looking at right now was built as part 
+					as an assignment for COMP213 at Centennial College, and it was 
+					hosted on <a href="http://studentweb.cencol.ca/">studentweb.cencol.ca</a>. 
+					I have now expanded it into my own little site to host some 
+					of my past works and assignments I did as a student at Centennial 
+					and UofT; a writer with 24OurMusic, Amp the Sound, and Feed My 
+					Curiosity, which I am currently writing scripts for; and of 
+					course, my resume!
 				</p>
 
 				<p>
-					This page is looking bland right now, but I hope to dress it up with stuffs like a navigation bar to make 
-					this feel more like home.
+					This page is looking bland right now, but I hope to dress it 
+					up with stuffs like a navigation bar to make this feel more like home.
 				</p>
 			</div>
 
