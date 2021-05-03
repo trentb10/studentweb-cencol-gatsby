6466d269d2b8f32bb3420f2da6984da2152c42d0
trentb10 <trentbmin@gmail.com>
2021-05-03 15:21:36 -0400

    Made further changes to trentb10-style and navi

--- src/components/navi.js
+++ src/components/navi.js
@@ -3,26 +3,26 @@ import { Link } from 'gatsby';
 
 export default function Header() {
     return (
-        <nav id="mainNavi">
-            <ul id="ulNavi">
+        <nav class="mainNavi">
+            <ul class="ulNavi">
                 <li class="title">tm-</li>
-                <li><Link to="/"><div class="item" href="#">Home</div></Link></li>
-                <li><Link to="/"><div class="item" href="#">About Me</div></Link></li>
+                <li><Link to="/"><div class="item">Home</div></Link></li>
+                <li><Link to="/"><div class="item">About Me</div></Link></li>
                 <li class="dropdown">
-                    <Link to="/assignments"><div href="#" class="dropbtn">Assignments</div></Link>
+                    <Link to="/assignments"><div class="mainNavi dropbtn">Assignments</div></Link>
                     <div class="dropdown-content">
-                        <Link to=""><div href="#">Centennial College</div></Link>
-                        <Link to=""><div href="#">University of Toronto</div></Link>
+                        <Link to=""><div>Centennial College</div></Link>
+                        <Link to=""><div>University of Toronto</div></Link>
                     </div>
                 </li>
                 <li class="dropdown">
-                    <Link to="/"><div id="mainNavi" class="dropbtn">Works</div></Link>
+                    <Link to="/"><div class="dropbtn">Works</div></Link>
                     <div class="dropdown-content">
-                        <Link to=""><div href="#">24Our Music</div></Link>
-                        <Link to=""><div href="#">Amp the Sound</div></Link>
-                        <Link to=""><div href="#">Dollar Gamers</div></Link>
-                        <Link to=""><div href="#">Feed My Curiosity</div></Link>
-                        <Link to=""><div href="#">Personal Works</div></Link>
+                        <Link to=""><div>24Our Music</div></Link>
+                        <Link to=""><div>Amp the Sound</div></Link>
+                        <Link to=""><div>Dollar Gamers</div></Link>
+                        <Link to=""><div>Feed My Curiosity</div></Link>
+                        <Link to=""><div>Personal Works</div></Link>
                     </div>
                 </li>
             </ul>
