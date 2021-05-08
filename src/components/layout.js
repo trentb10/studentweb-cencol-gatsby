08e38b583dca504b03ce9613e4614240a6f695c2
trentb10 <trentbmin@gmail.com>
2021-05-08 00:32:37 -0400

    Rearranged layout so header image is at the top

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,9 +20,9 @@ export default function Layout ({ title, children }) {
     
     return (
         <div>
+            <HeaderImage image = {image} />
             <Navi />
             <HeaderTitle title = {title} />
-            <HeaderImage image = {image} />
 
             <div class="content-body">
                 { children }
