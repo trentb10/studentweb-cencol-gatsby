b558d2a880bf018072c5dd2d2e2532b4165ee237
trentb10 <trentbmin@gmail.com>
2021-05-07 22:32:41 -0400

    Added multiple header images to randomize through

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,14 +7,21 @@ import Footer from './footer';
 
 import style from '../styles/trentb10-style.css';
 
-import image from '../images/logo.gif';
+import HeaderImage1 from '../images/header-1.png';
+import HeaderImage2 from '../images/header-2.png';
+import HeaderImage3 from '../images/header-3.png';
+import HeaderImage4 from '../images/header-4.png';
+import HeaderImage5 from '../images/header-5.png';
+import HeaderImage6 from '../images/header-6.png';
 
 export default function Layout ({ title, children }) {
+    let image = randomizeImage();
+    
     return (
         <div>
             <Navi />
-            <HeaderImage image = {image} />
             <HeaderTitle title = {title} />
+            <HeaderImage image = {image} />
 
             <div class="content-body">
                 { children }
@@ -23,6 +30,34 @@ export default function Layout ({ title, children }) {
             <Footer />
         </div>
     );
+}
+
+function randomizeImage() {
+    let number = Math.floor(Math.random() * 6) + 1;
+    let image;
+
+    switch (number) {
+        case 1:
+            image = HeaderImage1;
+            break;
+        case 2:
+            image = HeaderImage2;
+            break;
+        case 3:
+            image = HeaderImage3;
+            break;
+        case 4:
+            image = HeaderImage4;
+            break;
+        case 5:
+            image = HeaderImage5;
+            break;
+        case 6:
+            image = HeaderImage6;
+            break;
+    }
+
+    return image;
 }
 
 /**
