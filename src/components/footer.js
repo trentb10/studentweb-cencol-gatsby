f654034406916ca03c1f34977d5c28b7d31aa5f5
trentb10 <trentbmin@gmail.com>
2023-09-13 20:57:18 -0400

    Forgot to save changes for footer.js file

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div class="footer-content">
-      <p>&copy;&nbsp;Trent B on Gatsby! Version 1.1.2 2019 - 2023</p>
+      <p>&copy;&nbsp;Trent B on Netlify! Version 1.1.2 2019 - 2023</p>
     </div>
   );
 }
