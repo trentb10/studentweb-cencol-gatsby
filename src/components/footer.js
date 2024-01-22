36fcaeaca1d6035a00d8bb211e793e06a45479bc
trentb10 <trentbmin@gmail.com>
2024-01-21 22:37:37 -0500

    Updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div class="footer-content">
-      <p>&copy;&nbsp;Trent B on Netlify! Version 1.1.2 2019 - 2023</p>
+      <p>&copy;&nbsp;Trent B on Netlify! Version 1.2 2019 - 2024</p>
     </div>
   );
 }
